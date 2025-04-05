98505aa6552cbe252cc24d53097ad945d6647d7c
Andrew <huli-gan_94@mail.ru>
2025-04-05 19:13:42 +0300

    Добавил компонент userSlice. Для состояний .

---
+++ src/app/slices/userSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    name: "",
+    progress: 0,
+    isModalOpen: false,
+};
+
+const userSlice = createSlice ({
+    name: 'user',
+    initialState,
+    reducers: {
+        setName: (state, action) => {
+            state.name = action.payload;
+        },
+        setProgress: (state, action) => {
+            state.progress = action.payload;
+        },
+        toggleModal: (state) => {
+            state.isModalOpen = !state.isModalOpen;
+        },
+    }
+});
+
+export const { setName, setProgress, toggleModal } = userSlice.actions;
+export default userSlice.reducer;
