d67689a3973b58d7208c344aef92543e2a090000
Andrew <huli-gan_94@mail.ru>
2025-04-30 16:01:40 +0300

    Рефакторинг userSlice добавил ачивки, id и чтобы имя сохранялось в localStorage

--- src/app/slices/userSlice.ts
+++ src/app/slices/userSlice.ts
@@ -2,7 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   name: "",
+  userId: null,
   progress: 0,
+  achievements: [],
   isModalOpen: false,
 };
 
@@ -12,15 +14,40 @@ const userSlice = createSlice({
   reducers: {
     setName: (state, action) => {
       state.name = action.payload;
+      if (typeof window !== "undefined") {
+        localStorage.setItem("userName", action.payload);
+      }
+    },
+    setUserId: (state, action) => {
+      state.userId = action.payload;
     },
     setProgress: (state, action) => {
       state.progress = action.payload;
     },
+    setAchievements: (state, action) => {
+      state.achievements = action.payload;
+    },
     toggleModal: (state) => {
       state.isModalOpen = !state.isModalOpen;
+    },
+    initializeFromLocalStorage: (state) => {
+      if (typeof window !== "undefined") {
+        const storedName = localStorage.getItem("userName");
+        if (storedName) {
+          state.name = storedName;
+        }
+      }
     },
   },
 });
 
-export const { setName, setProgress, toggleModal } = userSlice.actions;
+export const {
+  setName,
+  setUserId,
+  setProgress,
+  setAchievements,
+  toggleModal,
+  initializeFromLocalStorage,
+} = userSlice.actions;
+
 export default userSlice.reducer;
