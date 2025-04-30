2fab0fa39e2b7976feb32a65766c31f1cc921260
Andrew <huli-gan_94@mail.ru>
2025-04-30 21:05:34 +0300

    Добавил страницу с достижениями и Link в header

---
+++ src/app/achievementsPage/page.tsx
@@ -0,0 +1,12 @@
+const AchievementsPage = () => {
+  return (
+    <>
+      <div className="w-full max-w-6xl flex flex-col md:flex-row items-center gap-12 py-20">
+        <div className="">
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AchievementsPage;
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,8 +1,10 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import { RootState } from "@/app/slices/Store";
 import { initializeFromLocalStorage } from "@/app/slices/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import Link from "next/link";
 
 const Header = () => {
   const [activePage, setActivePage] = useState("about");
@@ -13,15 +15,15 @@ const Header = () => {
 
   // Восстанавливаем данные из localStorage при первой загрузке
   useEffect(() => {
-    dispatch(initializeFromLocalStorage())
-  },[dispatch]);
+    dispatch(initializeFromLocalStorage());
+  }, [dispatch]);
 
   const menuItems = [
-    { name: "Обо мне", id: "about" },
-    { name: "Навыки", id: "skills" },
-    { name: "Достижения", id: "achievements" },
-    { name: "Проекты", id: "projects" },
-    { name: "Контакты", id: "contact" },
+    { name: "Обо мне", id: "about", href: "/" },
+    { name: "Навыки", id: "skills", href: "/skillsPage" },
+    { name: "Достижения", id: "achievements", href: "/achievementsPage" }, // Добавляем href для страницы достижений
+    { name: "Проекты", id: "projects", href: "/projectsPage" },
+    { name: "Контакты", id: "contact", href: "/contactPage" },
   ];
 
   return (
@@ -31,7 +33,7 @@ const Header = () => {
         <a className="block text-black text-xl font-bold" href="#">
           My Portfolio
         </a>
-
+        {/* Навигация */}
         <nav className="hidden md:block">
           <ul className="flex items-center gap-6 text-sm">
             {menuItems.map((item) => (
@@ -40,13 +42,15 @@ const Header = () => {
                 className="relative group px-3 py-2 cursor-pointer"
                 onClick={() => setActivePage(item.id)}
               >
-                <span
-                  className={`relative text-gray-600 transition-colors duration-300 ${
-                    activePage === item.id ? "text-black" : "hover:text-black"
-                  }`}
-                >
-                  {item.name}
-                </span>
+                <Link href={item.href}>
+                  <span
+                    className={`relative text-gray-600 transition-colors duration-300 ${
+                      activePage === item.id ? "text-black" : "hover:text-black"
+                    }`}
+                  >
+                    {item.name}
+                  </span>
+                </Link>
                 <span
                   className={`absolute left-0 top-0 h-[2px] bg-black transition-all duration-300 ease-in-out ${
                     activePage === item.id ? "w-full" : "w-0 group-hover:w-full"
@@ -56,10 +60,9 @@ const Header = () => {
             ))}
           </ul>
         </nav>
+        {/* Имя HR */}
         <div className="ml-auto flex items-center gap-4">
-            <span className="text-xl font-bold text-black">
-                {hrName}
-            </span>
+          <span className="text-xl font-bold text-black">{hrName}</span>
         </div>
       </div>
     </header>
