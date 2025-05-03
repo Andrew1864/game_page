1cc6cf2ff15a079bdbd4dead11df113ea3f550d7
Andrew <huli-gan_94@mail.ru>
2025-05-03 18:28:47 +0300

    Добавлена логика объединения локального списка items с глобальными достижениями из Redux (achievements). Это позволяет отображать актуальный статус достижений (завершены или нет) в пользовательском интерфейсе.

--- src/app/achievementsPage/page.tsx
+++ src/app/achievementsPage/page.tsx
@@ -46,6 +46,18 @@ const items = [
 
 const AchievementsPage = () => {
   const hrName = useSelector((state: RootState) => state.user.name);
+  const achievements = useSelector(
+    (state: RootState) => state.user.achievements
+  );
+  const totalPoints = achievements.filter((e) => e.completed).length * 10;
+
+  const mergedItems = items.map((item) => {
+    const achievement = achievements.find((a) => a.title === item.title);
+    return {
+      ...item,
+      completed: achievement ? achievement.completed : false,
+    };
+  });
 
   return (
     <>
@@ -61,13 +73,14 @@ const AchievementsPage = () => {
             </p>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               <CelebrationIcon /> Твои очки за прохождение:
+              {totalPoints}
             </p>
           </div>
         </div>
         <div className="w-full border-b border-gray-300 mt-6"></div>
         <h4 className="mb-2 text-2xl font-bold">Список достижений.</h4>
         <div className="w-full max-w-6xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {items.map((item, index) => (
+          {mergedItems.map((item, index) => (
             <AchievementCard
               key={index}
               title={item.title}
