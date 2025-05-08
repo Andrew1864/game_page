baf66944461e8a4d0022b202c59e6335815f4b06
Andrew <huli-gan_94@mail.ru>
2025-05-08 18:19:54 +0300

    Добавил страницу MaryShop

---
+++ src/app/Maryshop/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
+// import Gallery from "../components/GalleryPhoto/Gallery";
+// import VideoPlayer from "../components/GalleryVideo/VideoPlayer";
+
+const Maryshop = () => {
+  return (
+    <>
+      <div className="bg-gray-100">
+        <div className="container mx-auto px-4 py-8">
+          <div className="flex flex-wrap -mx-4">
+            {/* Левая часть с изображениями */}
+            <div className="w-full md:w-1/2 px-4 mb-8">
+              {/* <VideoPlayer /> */}
+              <div className="flex gap-4 py-4 justify-center overflow-x-auto">
+                {/* <Gallery /> */}
+              </div>
+            </div>
+            {/* Правая часть с текстом */}
+            <div className="w-full md:w-1/2 px-4">
+              <h2 className="text-3xl font-bold mb-4">Maryshop</h2>
+              <p className="text-gray-700 mb-6 text-lg">
+                В этом проекте реализовано - корзина товаров. Тут есть и
+                регистрация, так же есть JSON-server. Добавление и удаление из
+                корзины товаров.
+              </p>
+              <div>
+                <h3 className="text-lg font-semibold mb-2">
+                  Для запуска проекта локально:
+                </h3>
+                <ul className="list-disc list-inside text-gray-700">
+                  <li>
+                    Установка зависимостей: <b>npm install</b>
+                  </li>
+                  <li>
+                    Запуск сервера: <b>npm install</b>
+                  </li>
+                  <li>
+                    Запуск проекта: <b>npm run dev</b>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex items-center gap-2 mt-6 mb-3">
+                <a
+                  href="https://github.com/Andrew1864/InfoNews/tree/Andrew"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center justify-center w-16 h-16 bg-gray-200 rounded-xl shadow-md cursor-pointer hover:bg-gray-300 transition"
+                >
+                  <GitHubIcon className="w-8 h-8 text-gray-800" />
+                </a>
+                <span className="text-lg font-semibold text-gray-700">
+                  GitHub
+                </span>
+              </div>
+              <div className="flex gap-6 mt-3">
+                <div className="flex items-center justify-center w-16 h-16 bg-gray-200 rounded-xl shadow-md cursor-pointer hover:bg-gray-300 transition">
+                  <ThumbUpOffAltIcon className="w-8 h-8 text-green-600" />
+                </div>
+                <div className="flex items-center justify-center w-16 h-16 bg-gray-200 rounded-xl shadow-md cursor-pointer hover:bg-gray-300 transition">
+                  <ThumbDownOffAltIcon className="w-8 h-8 text-red-600" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Maryshop;
--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -233,9 +233,12 @@ const HomeComponents: React.FC = () => {
             </p>
             <button className="mt-4 inline-block px-8 py-3 border-2 border-black rounded-xl font-semibold transition-all duration-300 relative overflow-hidden group cursor-pointer text-black">
               <span className="absolute inset-0 bg-black w-0 group-hover:w-full transition-all duration-300"></span>
-              <span className="relative z-10 group-hover:text-white">
+              <Link
+                href="/Maryshop"
+                className="relative z-10 group-hover:text-white"
+              >
                 Посмотреть проект
-              </span>
+              </Link>
             </button>
           </div>
         </div>
