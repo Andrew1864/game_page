d5261d7b85b3a6347b3f8aa9234ef1131ceda529
Andrew <huli-gan_94@mail.ru>
2025-08-05 17:52:07 +0300

    Добавлена обёртка Provider + PersistGate

--- src/app/components/ReduxProvider/ReduxProvider.tsx
+++ src/app/components/ReduxProvider/ReduxProvider.tsx
@@ -2,12 +2,19 @@
 
 import React from "react";
 import { Provider } from "react-redux";
-import store from "@/app/slices/Store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "@/app/slices/Store";
 
 const ReduxProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default ReduxProvider;
