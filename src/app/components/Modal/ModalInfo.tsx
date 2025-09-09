d063388b63ffb34f6c269dec90a1af606099e9a5
Andrew <huli-gan_94@mail.ru>
2025-09-09 15:59:19 +0300

    Добавил цвет описанию в модалке.

--- src/app/components/Modal/ModalInfo.tsx
+++ src/app/components/Modal/ModalInfo.tsx
@@ -39,7 +39,7 @@ const ModalInfo: React.FC<ModalInfoProps> = ({
     >
       <div className="bg-white p-6 rounded-xl w-[90%] sm:w-[400px] text-center border border-gray-300">
         <h2 className="text-xl font-bold mb-2">{title}</h2>
-        <p className="mb-4">{description}</p>
+        <p className="mb-4 text-gray-700">{description}</p>
         <button
           onClick={onClose}
           className="bg-gray-800 cursor-pointer text-white px-4 py-2 rounded-xl w-full hover:bg-gray-900 transition"
